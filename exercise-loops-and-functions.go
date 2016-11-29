b2d638dbe4fa5d3f057701a5173a7d88e2f5f44b
Andrew Morgan <amorgan.email@gmail.com>
2016-11-29 17:44:17 -0500

    working solution

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func Sqrt(x float64) float64 {
+const diff = 0.001
 
+func Sqrt(x float64) float64 {
+	
+	z     := 1.0
+	z0    := 0.0
+	delta := 1.0
+		
+	for delta > diff {
+		z0 = z
+		z  = z - (z*z - x) / (2*z) 
+		delta = math.Abs(z - z0)
+	}
+
+	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+
+	fmt.Println(Sqrt(16))
 }
 
 
