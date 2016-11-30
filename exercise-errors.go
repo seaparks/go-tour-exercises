0e710768c4e3be9a0bb63c70b12a6e3e60e9e3a7
Andrew Morgan <amorgan.email@gmail.com>
2016-11-30 16:32:38 -0500

    working solution

--- exercise-errors.go
+++ exercise-errors.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+const diff = 0.001
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %g", float64(e))
+}
+
 func Sqrt(x float64) (float64, error) {
-	return 0, nil
+	
+	//complex numbers not supported!
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	
+	z     := 1.0
+	z0    := 0.0
+	delta := 1.0
+		
+	for delta > diff {
+		z0 = z
+		z  = z - (z*z - x) / (2*z) 
+		delta = math.Abs(z - z0)
+	}
+
+	return z, nil
 }
 
 func main() {
 	fmt.Println(Sqrt(2))
 	fmt.Println(Sqrt(-2))
 }
+
