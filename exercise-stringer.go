e8baafd2710fdfb7e41af7ebd96dfee350fc152e
Andrew Morgan <amorgan.email@gmail.com>
2016-11-30 15:53:12 -0500

    working solution

--- exercise-stringer.go
+++ exercise-stringer.go
@@ -6,6 +6,10 @@ type IPAddr [4]byte
 
 // TODO: Add a "String() string" method to IPAddr.
 
+func (i IPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", i[0], i[1], i[2], i[3])
+}
+
 func main() {
 	hosts := map[string]IPAddr{
 		"loopback":  {127, 0, 0, 1},
